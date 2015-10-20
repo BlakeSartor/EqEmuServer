02006afbce13989d6482e1623337a7835ca31fb7
Blake Sartor <sarto019@umn.edu>
2015-10-19 22:50:30 -0500

    modified battleground_bob.pl

--- timorous/Battleground_Bob.pl
+++ timorous/Battleground_Bob.pl
@@ -1,11 +1,10 @@
 sub EVENT_SAY {
     if ($text=~/hail/i) {
-	quest::say("Hail $name. Let me know if you would like to [" . quest::saylink("join") . "] the fight.");
-	quest::popup("Test");
+	quest::say("Hail $name. We are looking for mighty level 50 adventurers such as yourself to join the war! Let me know if you would like to [" . quest::saylink("join") . "] the fight. Help fight for the future of norrath!");
     }
     elsif ($text=~/join/i) {
 	if ($ulevel != 50) {
-	    quest::say("I'm sorry $name, but you must be level 50 to participate in this battleground.");
+	    quest::say("I'm sorry $name, but you must be level 50 to participate in this battleground. Speak with Grabriel the Third if you like to learn more about gaining the powers of a level 50.");
 	}
 	else {
 	quest::pvp("1");
