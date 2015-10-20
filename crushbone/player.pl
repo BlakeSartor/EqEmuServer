ed5e3d42d89c6a64c49c7cee1c05095983c434df
Blake Sartor <sarto019@umn.edu>
2015-10-20 15:04:36 -0500

    modified player.pl

--- crushbone/player.pl
+++ crushbone/player.pl
@@ -1,14 +1,19 @@
 $count = 0;
+$battle = 0;
 sub EVENT_ENTERZONE {
     $client->Message(4, "Waiting for other players to join. Please be patient...");
     $count++;    
         $client->Message(4, $count);
-    if ($count >= 2) {
+    if ($count >= 2 && $battle = 0) {
 	quest::shout("Battle starting in 5 minutes...");
+	$battle = 1;
     }
 
 }
 
 sub EVENT_ZONE {
     $count--;
+    if ($count 1) {
+	quest::shout("WINNER WINNER CHICKEN DINNER");
+    }
 }
