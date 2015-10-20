c6bf88e0a18f4c506b07e604204c3bd60c6f19f0
Blake Sartor <sarto019@umn.edu>
2015-10-20 17:10:26 -0500

    modified crushbone/player.pl

--- crushbone/player.pl
+++ crushbone/player.pl
@@ -5,7 +5,7 @@ sub EVENT_ENTERZONE {
     $count++;    
     $client->Message(4, $count);
     $client->Message(4, $battle);
-    my $some_boss = $entity_list->GetNPCByNPCTypeID(999242); # Example
+    my $some_boss = $entity_list->GetNPCByID(999242); # Example
     $some_boss->Say("hi");
     quest::shout($some_boss);
     if ($count == 2 && $battle == 0) {
