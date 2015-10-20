1124ad43bca6cc71a6b7a752014a09221130827d
Blake Sartor <sarto019@umn.edu>
2015-10-20 01:44:47 -0500

    added chad.pl

--- timorous/chad.pl
+++ timorous/chad.pl
@@ -7,8 +7,7 @@ sub EVENT_SAY {
     $z = $client->GetZ();
     $corpse = quest::getplayerburriedcorpsecount($charid);
     if($text=~/Hail/i) {
-        quest::emote("The spectral figure stares into your soul with 
-cold eyes, its voice entering your mind.");
+        
 	quest::say("We keep all that has been lost. In times past, nothing be returned once it was mine. Time have changed. The bodies, your [" . quest::saylink("bodies") . "], that were lost to you can now be returned.");
     }
     if($text=~/bodies/i && $corpse == 0) {
