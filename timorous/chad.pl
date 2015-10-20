d572064d655fe199712594ba891bce87a5b773bf
Blake Sartor <sarto019@umn.edu>
2015-10-20 02:49:51 -0500

    added chad.pl

--- timorous/chad.pl
+++ timorous/chad.pl
@@ -1,9 +1,6 @@
 sub EVENT_SAY {
     my $CorpseCount = 0;
     my $charid = $client->CharacterID();
-    my $x = $client->GetX();
-    my $y = $client->GetY();
-    my $z = $client->GetZ();
     if($text=~/hail/i) {
 	$client->Message(15,"I can [" . quest::saylink("bury a corpse") . "] or [" . quest::saylink("summon a corpse") . "] that you have buried.");
     } else {
