2f1cab2174913dcdeb1b4ed5ffef354bf15169bf
Blake Sartor <sarto019@umn.edu>
2015-10-20 01:55:32 -0500

    added chad.pl

--- timorous/chad.pl
+++ timorous/chad.pl
@@ -2,6 +2,6 @@ sub EVENT_SAY {
     if ($text=~/hail/i) {
 	quest::emote("The spectral figure stares into your soul with 
 cold eyes, its voice entering your mind.");
-	quest::say("We keep all that has been lost. In times past, nothing be returned once it was mine. Time have changed. The bodies, your [" . quest::saylink("bodies") . "], that were lost to you can now be returned.");
+	quest::say("We keep all that has been lost. In times past, nothing be returned once it was mine. Times have changed. The bodies, your [" . quest::saylink("bodies") . "], that were lost to you can now be returned.");
     }
 }
