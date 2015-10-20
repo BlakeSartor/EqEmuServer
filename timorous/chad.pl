a50876d27d9e1ab1032c4aac609979d66741e0a5
Blake Sartor <sarto019@umn.edu>
2015-10-20 03:01:50 -0500

    added chad.pl

--- timorous/chad.pl
+++ timorous/chad.pl
@@ -2,29 +2,28 @@ sub EVENT_SAY {
     my $CorpseCount = 0;
     my $charid = $client->CharacterID();
     if($text=~/hail/i) {
-	$client->Message(15,"I can [" . quest::saylink("bury a corpse") . "] or [" . quest::saylink("summon a corpse") . "] that you have buried.");
+	$client->Message(15,"I can [" . quest::saylink("summon a corpse") . "] .");
     } else {
 	$CorpseCount = COUNT_CORPSES();
-	if($text eq "bury a corpse" || $text eq "bury the corpse") {
+
+	if($text eq "summon a corpse" || $text eq "summon the corpse") {
 	    if($CorpseCount > 0) {
-		quest::buryplayercorpse($charid);
-		$client->Message(15,"Very well, burying one of your corpses now.");
+		quest::summonallplayercorpses($charid, $x, $y, $z, 0);
+		$client->Message(15,"Very well, summoning your corpses now.");
 	    } else {
-		$client->Message(13,"You have no unburied corpses, begone.");
+		$client->Message(13,"You have no corpses, begone.");
 	    }
-	}
-	elsif ($text eq "summon a corpse" || "summon the corpse") {
-		quest::say("Attempting to summon buried corpses2.");
-		quest::summonallplayercorpses($charid, $x, $y, $z, 0); 
+	}	
+    }
+}
+
+sub COUNT_CORPSES {
+    my @corpse_list = $entity_list->GetCorpseList();
+    my $count = 0;
+    foreach $corpse_found (@corpse_list) {
+	if ($corpse_found->GetOwnerName() eq $client->GetName()) {
+	    $count++;
 	}
     }
-    sub COUNT_CORPSES {
-	my @corpse_list = $entity_list->GetCorpseList();
-	my $count = 0;
-	foreach $corpse_found (@corpse_list) {
-	    if ($corpse_found->GetOwnerName() eq $client->GetName()) {
-		$count++;
-	    }
-	}
-	$count;
-    }
+    $count;
+}
