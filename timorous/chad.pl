78ffcc43922b0c28040b81ffb3e66a77459c6c11
Blake Sartor <sarto019@umn.edu>
2015-10-20 01:46:26 -0500

    added chad.pl

--- timorous/chad.pl
+++ timorous/chad.pl
@@ -10,13 +10,13 @@ sub EVENT_SAY {
         
 	quest::say("We keep all that has been lost. In times past, nothing be returned once it was mine. Time have changed. The bodies, your [" . quest::saylink("bodies") . "], that were lost to you can now be returned.");
     }
-    if($text=~/bodies/i && $corpse == 0) {
+    elsif($text=~/bodies/i && $corpse == 0) {
 	quest::emote("The voice booms angrily in your mind.");
 	quest::say("Be gone from this place, you waste my time!");
 	$corpse = 0;
 	$charid = 0;
     }
-    if($text=~/bodies/i && $corpse > 0) {
+    elsif($text=~/bodies/i && $corpse > 0) {
 	quest::summonburriedplayercorpse($charid, $x, $y, $z, 0);
 	$corpse = 0;
 	$charid = 0;
