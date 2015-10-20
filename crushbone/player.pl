7f79f77f6dcc5c1abdb8f192fe087b8ef9b15f48
Blake Sartor <sarto019@umn.edu>
2015-10-20 16:58:36 -0500

    lk

--- crushbone/player.pl
+++ crushbone/player.pl
@@ -1,12 +1,12 @@
 $count = 0;
 $battle = 0;
 $some_boss = $entity_list->GetNPCByNPCTypeID(999242); # Example
-$some_boss->Shout("hi");
 sub EVENT_ENTERZONE {
     $client->Message(4, "Waiting for other players to join. Please be patient...");
     $count++;    
     $client->Message(4, $count);
     $client->Message(4, $battle);
+    $some_boss->Shout("hi");
     if ($count == 2 && $battle == 0) {
 	quest::shout("Battle starting in 5 minutes...");
 	$battle = 1;
