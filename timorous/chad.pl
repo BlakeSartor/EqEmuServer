6ce252481aa45343d263f97803e7d9162b54f3d9
Blake Sartor <sarto019@umn.edu>
2015-10-20 02:01:08 -0500

    added chad.pl

--- timorous/chad.pl
+++ timorous/chad.pl
@@ -5,7 +5,6 @@ cold eyes, its voice entering your mind.");
 	quest::say("We keep all that has been lost. In times past, nothing be returned once it was mine. Times have changed. The bodies, your [" . quest::saylink("bodies") . "], that were lost to you can now be returned.");
     }
     elsif ($text=~/bodies/i) {
-    quest::summonburriedplayercorpse(($client->CharacterID()), ($client->GetX()), ($client->GetY()), ($client->GetZ()), 0);
     quest::say("attemping to summon corpses.");
 
     }
