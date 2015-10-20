1dc4a1f7aaf73f42509ef1c1516175d9a94d3f4e
blake <blakesartor@gmail.com>
2015-10-20 14:25:11 -0500

    modiied default.pl

--- crushbone/default.pl
+++ crushbone/default.pl
@@ -1,18 +1,3 @@
 sub EVENT_ENTERZONE {
-#this is all the contested zones
-    my @zonelist = qw(crushbone);
-    if ( grep { $_ eq $zonesn } @zonelist ) { #Save PVP state if you zone into a contested zone
-	quest::setglobal("PvPState",$client->GetPVP(),5,"F"); #Adds their PVPstate to global
-	$client->SetPVP(1);
-    }
-
-}
-
-sub EVENT_ZONE {
-    my @zonelist = qw(crushbone);
-    if ( grep { $_ eq $zonesn } @zonelist ) { #If you zone out a contested zone
-	if(defined $qglobals{"PvPState"} && $qglobals{"PvPState"}==0) { #Checks global PVPState & Check if PvPState = 0
-	    $client->SetPVP(0) #Then turn pvp off
-	}
-    }
+	quest::shout("Hello");
 }
