f829891a5c7a98484a36304e9ad703580a96d8e5
Blake Sartor <sarto019@umn.edu>
2015-10-20 13:58:27 -0500

    added /crushbone/default.pl

--- crushbone/default.pl
+++ crushbone/default.pl
@@ -1,3 +1,35 @@
 sub EVENT_ENTERZONE {
-    $client->Message(4,"Hello and wlecome to the battle");
+    my $playerip = $client->GetIP();
+    @pvplist;
+    if ( grep { $_ eq $playerip } @pvplist )
+{
+
+    $client->Message(315,"YOU MAY ONLY HAVE ONE CHARACTER IN THIS ZONE!");
+    push(@pvplist, "$playerip");
+    $client->MovePC(348, 1.8, 1.6, -22.8, 187);
+    return;
+}
+push(@pvplist, "$playerip");
+$client->SetPVP(1);
+}
+
+sub EVENT_ZONE {
+    my $playerip = $client->GetIP();
+    my $count = 0;
+    foreach $pvp_player (@pvplist) 
+{
+    next unless $pvp_player = $playerip;
+    $count++;
+}
+
+if ($count >= 2)
+{
+    @pvplist = grep {$_ ne $playerip} @pvplist;
+    push(@pvplist, "$playerip"); #Add the IP back to list since there are 2 of the same IP
+}
+else
+{
+    @pvplist = grep {$_ ne $playerip} @pvplist;
+}
+$client->SetPVP(0);
 }
