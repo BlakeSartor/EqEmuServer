a31d75f41b52178bc22cf96153f8e61fbc8ee102
Blake Sartor <sarto019@umn.edu>
2015-10-19 22:37:01 -0500

    modified battleground_bob.pl

--- timorous/Battleground_Bob.pl
+++ timorous/Battleground_Bob.pl
@@ -3,8 +3,13 @@ sub EVENT_SAY {
 	quest::say("Hail $name. Let me know if you would like to [" . quest::saylink("join") . "] the fight.");
 	  quest::popup("Level 50 Battleground. <br><br>Join the fight!. <br><br> Hello adventurer. I can help you join the level 50 battleground. Just let me know if you would like to join the battle. Remember, once you join you cannot leave until you have been slain or the battle has ended.<br><br> Good luck!");  
     }
-    else if ($text=~/join/i) {
+    elsif ($text=~/join/i) {
+	if ($ulevel != 50) {
+	    quest::say("I'm sorry $name, but you must be level 50 to participate in this battleground.");
+	}
+	else {
 	quest::pvp("1");
 	quest::zone("77");
+	}
     }
 }
