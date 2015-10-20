db6833036ec9ca4a75d3ac75aa9ac4c9d6cf2626
Blake Sartor <sarto019@umn.edu>
2015-10-20 14:56:08 -0500

    modified player.pl

--- crushbone/player.pl
+++ crushbone/player.pl
@@ -3,5 +3,12 @@ sub EVENT_ENTERZONE {
     $client->Message(4, "Waiting for other players to join. Please be patient...");
     $count++;    
         $client->Message(4, $count);
+    if ($count >= 2) {
+	quest::shout("Battle starting in 5 minutes...");
+    }
 
+}
+
+sub EVENT_ZONE {
+    $count--;
 }
