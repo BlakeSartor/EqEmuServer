4b848f97e911a436c80c7833219c2b2428f1359d
Blake Sartor <sarto019@umn.edu>
2015-10-20 23:23:00 -0500

    modified crushbone/player.pl

--- crushbone/player.pl
+++ crushbone/player.pl
@@ -28,7 +28,7 @@ sub EVENT_ZONE {
     if ($count <= 1 && $battle == 1) {
 	$battle = 0;
 	$client->SetPVP(0);	
-	quest::settimer("t11",1);
+	quest::we(258,"gs");
 	
     }
     else {
@@ -91,7 +91,7 @@ sub EVENT_TIMER {
 	}
     }
     elsif ($timer eq "t11") {
-	quest::stoptimer("t11");
+	quest::stoptimer ("t11");
 	quest::we(258, "The battle has ended and $winnderID has emerged victorious!");
     }
 }
