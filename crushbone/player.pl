97dca54e22e4aabe83bd2b2403d05f672b77c355
Blake Sartor <sarto019@umn.edu>
2015-10-21 12:34:53 -0500

    modified crushbone/player.pl

--- crushbone/player.pl
+++ crushbone/player.pl
@@ -30,8 +30,8 @@ sub EVENT_ZONE {
 	$client->SetPVP(0);	
 	my @wlist = $entity_list->GetClientList();
 	foreach $w (@wlist) {
-	    $2ndplace = $client->CharacterID());
-	quest::we(258, "2nd place character id is $2ndplace");
+	    $2ndplace = $client->CharacterID();
+	    quest::we(258, "2nd place character id is $2ndplace");
 	    if ($w != $client->CharacterID()) {
 		$winnerID = $w->GetCleanName();
 		quest::we(258, "The battle for Crushbone has ended and $winnerID has emerged victorious!");
