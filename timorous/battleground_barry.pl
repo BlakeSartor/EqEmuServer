bc96a85411057ad86b49ba36322c4943b6127894
Blake Sartor <sarto019@umn.edu>
2015-10-24 22:05:24 -0500

    modified timorous/battleground_barry.pl

---
+++ timorous/battleground_barry.pl
@@ -0,0 +1,21 @@
+sub EVENT_SAY {
+    if ($text=~/hail/i) {
+	quest::say("Hail $name. We are looking for mighty level 50 adventurers such as yourself to join the war in plane of sky! Each player can use only items with knockback effects in attempt to knock all other players off the edge. Let me know if you would like to [" . quest::saylink("join") . "] the fight. Help fight for the future of norrath!");
+    }
+    elsif ($text=~/join/i) {
+	if ($ulevel != 50) {
+	    quest::say("I'm sorry $name, but you must be level 50 to participate in this battleground. Speak with Grabriel the Third if you like to learn more about gaining the powers of a level 50.");
+	}
+	else {
+	    quest::say("Attempting to place you in battleground...");
+	    if (defined $qglobals{"battleground_in_session"}) {
+		quest::say("I'm sorry $name, but a battleground is in session. Please wait for the current battleground to finish.");
+	    }
+	    else {
+		quest::redbind(71, 614.00, 1415.00, -663.62, 0);
+		$npc->Kill()
+		quest::pvp("off");
+	    }
+	}
+    }
+}
